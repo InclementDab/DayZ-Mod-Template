534e99d71b8fcc98ee29cf874518c467fe0d484a
Tyler Paul <tylerpaul2329@gmail.com>
2023-04-16 14:15:56 -0400

    this shit kinda sucks rn

--- ModTemplate/Workbench/Krypton/App.xaml.cs
+++ ModTemplate/Workbench/Krypton/App.xaml.cs
@@ -4,12 +4,9 @@ using System.Diagnostics;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Serilog;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
-//using ILogger = Microsoft.Extensions.Logging.ILogger;
-using ILogger = Serilog.ILogger;
+using Serilog;
+using Microsoft.Extensions.Configuration;
 
 namespace Krypton
 {
@@ -26,26 +23,18 @@ namespace Krypton
 
         public App()
         {
-        }
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.RollingFile(@"logs\Log-{Date}.log")
+                .MinimumLevel.Verbose()
+                .CreateLogger();
 
-        private void ConfigureServices(IServiceCollection services)
-        {
-            //.AddLogging(builder => builder.AddSerilog(dispose))
-            //services.AddSingleton(config);
-            services.AddTransient(typeof(ConfigurationWindow));
-            //.AddOptions()
-            //.AddLogging()
+            Log.Debug("Running app");
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             Options = LaunchOptions.CreateFromArgs(e.Args);
-
-
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
 
             IConfigurationBuilder config_builder = new ConfigurationBuilder()
                             .SetBasePath(AppContext.BaseDirectory)
@@ -55,12 +44,19 @@ namespace Krypton
             IConfiguration config = config_builder.Build();
             config["test"] = "123";
 
-            IServiceCollection services = new ServiceCollection();
-            ConfigureServices(services);
+            IServiceCollection services = new ServiceCollection()
+                // .AddLogging(logger)
+                .AddSingleton(config)
+                .AddTransient(typeof(ConfigurationWindow));
+            //.AddOptions()
+            //.AddLogging()
             ServiceProvider = services.BuildServiceProvider();
 
             ConfigurationWindow window = ServiceProvider.GetRequiredService<ConfigurationWindow>();
             window.Show();
+
+            //ILogger logger = ServiceProvider.GetRequiredService<ILogger>();
+            //logger.LogInformation("Test");
         }
     }
 }
