64cf2ec1ec69f5400af55415a61b3ebf3f7eb0d1
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-03 14:34:19 -0400

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -8,10 +8,10 @@ New-Item -Path "Missions\" -Name "Global" -ItemType Directory
 New-Item -Path "Missions\" -Name "Dev" -ItemType Directory
 
 # Individual script modules get their own sub-folders for wb organization
-New-Item -Path "Scripts\1_Core" -Name $prefix -ItemType Directory
-New-Item -Path "Scripts\3_Game" -Name $prefix -ItemType Directory
-New-Item -Path "Scripts\4_World" -Name $prefix -ItemType Directory
-New-Item -Path "Scripts\5_Mission" -Name $prefix -ItemType Directory
+New-Item -Path "ModTemplate\Scripts\1_Core" -Name $prefix -ItemType Directory
+New-Item -Path "ModTemplate\Scripts\3_Game" -Name $prefix -ItemType Directory
+New-Item -Path "ModTemplate\Scripts\4_World" -Name $prefix -ItemType Directory
+New-Item -Path "ModTemplate\Scripts\5_Mission" -Name $prefix -ItemType Directory
 
 Write-Host "Renaming folders..."
 
@@ -21,9 +21,9 @@ foreach ($folder in $folders) {
         $new_name = $folder.Name -replace "ModTemplate", $prefix
 
         # Rename the folder
-        Rename-Item -Path $folder.FullName -NewName $new_name
-
-        #Write-Host "Renamed '$($folder.Name)' to '$new_name'" -ForegroundColor Green
+        #Rename-Item -Path $folder.FullName -NewName $new_name
+    
+        Write-Host "Renamed '$($folder.Name)' to '$new_name'" -ForegroundColor Green
     }
 }
 
@@ -35,11 +35,23 @@ while ($git_enable -ne 'y' -and $git_enable -ne 'n') {
 
 # Check the user's choice
 if ($git_enable -eq 'y') {
+    if (Test-Path ".git") {
+        Write-Host "Existing repository detected" -ForegroundColor Yellow
+        $remotes = git remote
+        if (-not $remotes) {
+            Write-Host "Repository had no remotes, removing..." -ForegroundColor Yellow
+        } else {
+            Write-Host "Remotes detected" -ForegroundColor Red
+        }
+
+    }
+
+
     # Initialize a new git repository
-    git init
+    #git init
 
     Write-Host "Initializing Git Repository..." -ForegroundColor Yellow
-    
+
 
     Write-Host "Git repository initialized successfully." -ForegroundColor Green
 }
