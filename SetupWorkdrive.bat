e116f54e61bdf40224fcc94fbad9fb9cf6ca6e0a
Tyler Paul <tylerpaul2329@gmail.com>
2023-12-01 14:47:35 -0500

    Workdrive setup is better than ever

--- SetupWorkdrive.bat
+++ SetupWorkdrive.bat
@@ -1,12 +1,19 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+SET "drive=P:\"
+:check
+IF NOT EXIST %drive% (
+	SET /p "drive=Enter Drive Letter (P:\):"
+	goto check
+)
+
 REM Loop through all subdirectories in the current directory
 FOR /D %%D IN (*) DO (
     REM Check for gproj file
     IF EXIST "%%D\Workbench\dayz.gproj" (
         REM Create a junction between the "Workbench" folder and P:\FolderName
-        SET "junctionPath=P:\%%~nxD"
+        SET "junctionPath=%drive%\%%~nxD"
         ECHO Creating junction for "%%D" to "!junctionPath!"
         MKLINK /J "!junctionPath!" "%%D"
     )
