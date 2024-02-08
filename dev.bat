175944fe94254e21e4fba5558200d86d2e3576eb
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-07 20:15:46 -0500

    Update dev.bat

--- dev.bat
+++ dev.bat
@@ -1,5 +1,6 @@
 @echo off
 
+SetupWorkdrive.bat
 set repository=%cd%
 
 cd ModTemplate
@@ -9,6 +10,5 @@ taskkill /f /im "workbenchApp.exe"
 for /f "tokens=2,*" %%a in ('reg query "HKCU\SOFTWARE\Bohemia Interactive\Dayz Tools" /v "path" 2^>nul') do (
     set "dayz_tools=%%b"
 )
-
 
 "%dayz_tools%\Bin\Workbench\workbenchApp.exe" "-mod=P:\DabsFramework;P:\ModTemplate -profiles=P:\Profiles\Workbench -repository=%repository%"
